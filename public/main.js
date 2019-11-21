b37e90994f68b9e782daafb2024e9cbb06cbffcb
Lcousins <lucyrcousins@gmail.com>
2019-11-21 14:03:11 -0500

    edits to sin wave and footer
    edits to sin wave and footer

--- public/main.js
+++ public/main.js
@@ -1,13 +1,24 @@
 let xspacing = 16; // Distance between each horizontal location
 let w; // Width of entire wave
 let theta = 0.0; // Start angle at 0
-let amplitude = 66.0; // Height of wave
+let amplitude = 150.0; // Height of wave
 let period = 650.0; // How many pixels before the wave repeats
 let dx; // Value for incrementing x
 let yvalues; // Using an array to store height values for the wave
+let windowHeight = 200; //sets height of sine wave to 200px
 
 function setup() {
-  createCanvas(windowWidth, 200);
+  let canvas = createCanvas(windowWidth, windowHeight);
+  w = width + 20;
+  dx = (TWO_PI / period) * xspacing;
+  yvalues = new Array(floor(w / xspacing));
+  //windowResize();
+  //window.addEventListener("resize", windowResize);
+  canvas.parent('sketchHolder');
+ }
+
+function windowResized(){
+  resizeCanvas(windowWidth, windowHeight);
   w = width + 16;
   dx = (TWO_PI / period) * xspacing;
   yvalues = new Array(floor(w / xspacing));
@@ -27,16 +38,19 @@ function calcWave() {
   // For every x value, calculate a y value with sine function
   let x = theta;
   for (let i = 0; i < yvalues.length; i++) {
-    yvalues[i] = sin(x) * amplitude;
+    yvalues[i] = sin(x)/3 * amplitude;
     x += dx;
   }
 }
 
 function renderWave() {
-  noStroke();
-  fill(40,6,128);
+  stroke(40,6,128);
+  noFill();
+  strokeWeight(7);
   // A simple way to draw the wave with an ellipse at each location
+  beginShape();
   for (let x = 0; x < yvalues.length; x++) {
-    ellipse(x * xspacing, height / 2 + yvalues[x], 16, 16);
+    curveVertex(x * xspacing, height / 2 + yvalues[x]);
   }
+  endShape();
 }
