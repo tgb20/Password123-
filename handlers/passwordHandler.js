f6a0b5186fcdf16ec88230a3a7e10a5a602c9d64
Jacob Bashista <jacob@orange.haus>
2019-11-21 13:14:56 -0500

    Password Checking built into add password request

--- handlers/passwordHandler.js
+++ handlers/passwordHandler.js
@@ -16,7 +16,16 @@ module.exports = {
                 res.json({ success: false, error: err });
             } else {
                 console.log(password);
-                res.json({ success: true,  hash: password.hash});
+                HashedPassword.find({ hash: hash }, ((err, passwords) => {
+
+                    let unique = false;
+
+                    if (passwords.length > 0) {
+                        es.json({ success: false, hash: password.hash, error: "Password not unique"});
+                    } else {
+                        res.json({ success: true, hash: password.hash });
+                    }
+                }));
             }
         });
     }),
@@ -24,15 +33,15 @@ module.exports = {
     checkMatchingPassword: ((req, res) => {
         let hash = req.body.hash;
 
-        if(hash == null) {
-            res.json({error: "No hash provided"});
+        if (hash == null) {
+            res.json({ error: "No hash provided" });
         }
 
-        HashedPassword.find({hash: hash}, ((err, passwords) => {
-            if(passwords.length > 0) {
-                res.json({unique: false});
+        HashedPassword.find({ hash: hash }, ((err, passwords) => {
+            if (passwords.length > 0) {
+                res.json({ unique: false });
             } else {
-                res.json({unique: true});
+                res.json({ unique: true });
             }
         }));
     })
